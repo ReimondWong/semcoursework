72e5729bb3872062ed43b0a65c7dae2c2a92b25d
Orestis Zoidis (ozed-gr) <40428588@live.napier.ac.uk>
2020-02-12 22:49:22 +0000

    Added get and display Employee methods

--- src/main/java/com/napier/sem/Employee.java
+++ src/main/java/com/napier/sem/Employee.java
@@ -46,37 +46,5 @@ public class Employee
     public String manager;
 
     //Added extra parameter to fix issue of red highlighted con variable.
-    public Employee getEmployee(int ID, Connection con)
-    {
-        try
-        {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Return new employee if valid.
-            // Check one is returned
-            if (rset.next())
-            {
-                Employee emp = new Employee();
-                emp.emp_no = rset.getInt("emp_no");
-                emp.first_name = rset.getString("first_name");
-                emp.last_name = rset.getString("last_name");
-                return emp;
-            }
-            else
-                return null;
-        }
-        catch (Exception e)
-        {
-            System.out.println(e.getMessage());
-            System.out.println("Failed to get employee details");
-            return null;
-        }
-    }
+
 }
